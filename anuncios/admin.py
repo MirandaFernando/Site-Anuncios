793addc7e78a51f75b8d436409739e8006ed3421
Fernando <fernandomirandalolzeiro@gmail.com>
2021-03-05 21:47:26 -0300

    criado model anuncio

--- anuncios/admin.py
+++ anuncios/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from anuncios import models
 # Register your models here.
+admin.site.register(models.Categoria)
+admin.site.register(models.Anuncio)
--- anuncios/models.py
+++ anuncios/models.py
@@ -3,3 +3,14 @@ from django.db import models
 # Create your models here.
 class Categoria(models.Model):
     titulo = models.CharField(max_length=40)
+
+class Anuncio(models.Model):
+    titulo = models.CharField(max_length=40)
+    descricao = models.TextField(null=True, blank=True)
+    preco = models.DecimalField(max_digits=11, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    alterado_em = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.titulo
