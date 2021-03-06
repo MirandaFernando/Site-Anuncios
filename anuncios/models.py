df1deb1703cf172421691f3124a1cba82b271e22
Fernando <fernandomirandalolzeiro@gmail.com>
2021-03-05 21:23:45 -0300

    criado model categoria

--- anuncios/models.py
+++ anuncios/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    titulo = models.CharField(max_length=40)
--- anuncios/views.py
+++ anuncios/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Ola mundo")
+    return render(request, 'home.html')
