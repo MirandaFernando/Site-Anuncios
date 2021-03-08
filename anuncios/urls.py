e5452937bd1d312ce5b82ec396f366344b9c5928
Fernando <fernandomirandalolzeiro@gmail.com>
2021-03-08 16:58:37 -0300

    pagina de categoria

--- anuncios/urls.py
+++ anuncios/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from .views import home
+from .views import categorias
 
 
 urlpatterns = [   
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('categoria/<categoria_id>', categorias, name='categoria')
 ]
--- anuncios/views.py
+++ anuncios/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 
 from .models import Categoria
@@ -6,5 +7,13 @@ from .models import Anuncio
 
 def home(request):
     categorias = Categoria.objects.all()
-    anuncios = Anuncio.objects.all()
+    anuncios = Anuncio.objects.all()[:12]
     return render(request, 'home.html', {'categorias': categorias, 'anuncios': anuncios})
+
+def categorias(request, categoria_id):
+
+    categoria = get_object_or_404(Categoria, id=categoria_id  )
+    categorias = Categoria.objects.all()
+   
+    anuncios = Anuncio.objects.filter(categoria=categoria)  
+    return render(request, 'home.html', {'categorias': categorias, 'anuncios': anuncios,'categoria': categoria})
